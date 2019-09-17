b2e3760a5215ec4905787fba494cb5d3b8af181d
syshlang <158990605@qq.com>
2019-09-17 22:16:59 +0800

    singleton

---
+++ src/main/java/com/syshlang/designpatterns/creational/singleton/README.md
@@ -0,0 +1,5 @@
+# Singleton Design Pattern
+
+# Intent
+- Ensure a class has only one instance, and provide a global point of access to it.
+- Encapsulated "just-in-time initialization" or "initialization on first use".
--- src/main/java/com/syshlang/designpatterns/creational/singleton/package-info.java
+++ src/main/java/com/syshlang/designpatterns/creational/singleton/package-info.java
@@ -2,10 +2,6 @@
 /**
  * Singleton Design Pattern
  *
- * Ensure a class has only one instance, and provide a global point of access to it.
- * Encapsulated "just-in-time initialization" or "initialization on first use".
- *
- *
  */
 
 package com.syshlang.designpatterns.creational.singleton;
