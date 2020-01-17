9587b18fbe58832e9e1ad7932cca44c35cd9c50d
syshlang <sunyslang@gmail.com>
2020-01-17 09:00:17 +0800

    Design Pattern about  Prototype Pattern

---
+++ src/main/java/com/syshlang/designpatterns/creational/prototype/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2020.
+ * @File: package-info.java
+ * @Description:
+ * @Author: sunys
+ * @Date: 2020/1/17 上午8:59
+ * @since:
+ */
+
+package com.syshlang.designpatterns.creational.prototype;
