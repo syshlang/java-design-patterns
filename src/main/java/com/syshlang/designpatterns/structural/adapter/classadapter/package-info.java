9541ec3eae16f120e787091af180a59020791df5
syshlang <sunyslang@gmail.com>
2020-01-17 15:44:28 +0800

    Design Pattern about  Adapter Pattern

---
+++ src/main/java/com/syshlang/designpatterns/structural/adapter/classadapter/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2020.
+ * @File: package-info.java
+ * @Description:  类适配器模式
+ * @Author: sunys
+ * @Date: 2020/1/17 下午3:40
+ * @since:
+ */
+
+package com.syshlang.designpatterns.structural.adapter.classadapter;
---
+++ src/main/java/com/syshlang/designpatterns/structural/adapter/objectadapter/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2020.
+ * @File: package-info.java
+ * @Description:  对象适配器模式
+ * @Author: sunys
+ * @Date: 2020/1/17 下午3:41
+ * @since:
+ */
+
+package com.syshlang.designpatterns.structural.adapter.objectadapter;
