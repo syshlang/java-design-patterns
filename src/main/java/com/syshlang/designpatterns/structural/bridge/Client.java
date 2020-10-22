693b08b91661d822904718ea73a855b296ad06f4
sunys <sunyuansheng@grgbanking.com>
2020-10-22 09:16:35 +0800

    :sparkles: feat(post): new post
    new a post about Bridge Pattern

--- src/main/java/com/syshlang/designpatterns/structural/bridge/Client.java
+++ src/main/java/com/syshlang/designpatterns/structural/bridge/Client.java
@@ -15,6 +15,8 @@ import com.syshlang.designpatterns.structural.bridge.phone.PhoneBlack;
 import com.syshlang.designpatterns.structural.bridge.phone.PhoneRed;
 import com.syshlang.designpatterns.structural.bridge.phone.PhoneWhite;
 
+import java.sql.*;
+
 /**
  * The type Client.
  *
@@ -37,5 +39,33 @@ public class Client {
         phoneWhite.call();
         phoneWhite.sendMessage();
         phoneWhite.goOnline();
+    }
+
+    /**
+     * Jdbc test.
+     */
+    public static void jdbcTest(){
+        try {
+            // 1. 注册驱动
+            Class.forName("com.mysql.jdbc.Driver");
+            //2. 创建一个连接对象
+            Connection conn = DriverManager.getConnection("url","user","password");
+            //3. 创建一个sql语句的发送命令对象
+            Statement stmt = conn.createStatement();
+            // 4. 执行sql,拿到查询的结果集对象
+            ResultSet rs = stmt.executeQuery("s");
+            //5. 输出结果集的数据
+            while(rs.next()){
+                System.out.println(rs);
+            }
+            //6. 关闭连接，命令对象以及结果集。
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
